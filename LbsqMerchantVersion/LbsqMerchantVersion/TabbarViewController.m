008cb2254b3f5d17fc5b1ecb5c8e7d5e26199dcd
yan2531 <1028183856@qq.com>
2015-08-18 16:50:38 +0800

    自定义tabBar

--- LbsqMerchantVersion/LbsqMerchantVersion/TabbarViewController.m
+++ LbsqMerchantVersion/LbsqMerchantVersion/TabbarViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "TabbarViewController.h"
+#import "TabBarItem.h"
 
 @interface TabbarViewController ()
 
@@ -16,7 +17,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tabBar.hidden = YES;
+    
+    UIView *tabBarView = [[UIView alloc] initWithFrame:CGRectMake(0, kScreen_height-49, kScreen_width, 49)];
+    tabBarView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:tabBarView];
+    
+    for (int i = 0; i < 3; i++) {
+        if (i == 1) {
+            UIButton *scanBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+            scanBtn.frame = CGRectMake(0, 0, kScreen_width/3, tabBarView.height);
+            scanBtn.backgroundColor = KRandomColor;
+            scanBtn.tag = 100 + i;
+            scanBtn.centerX = tabBarView.centerX;
+            [scanBtn addTarget:self action:@selector(tabbarButtonsAction:) forControlEvents:UIControlEventTouchUpInside];
+            [tabBarView addSubview:scanBtn];
+        } else {
+            TabBarItem *itemView = [[TabBarItem alloc] initWithFrame:CGRectMake(i*kScreen_width/3, 0, kScreen_width/3, tabBarView.height) image:nil title:@"测试" selectedImage:nil];
+            itemView.button.tag = 100 + i;
+            [itemView.button addTarget:self action:@selector(tabbarButtonsAction:) forControlEvents:UIControlEventTouchUpInside];
+            [tabBarView addSubview:itemView];
+        }
+        
+    }
+    
     // Do any additional setup after loading the view.
+}
+
+-(void)tabbarButtonsAction:(UIButton *)button
+{
+    switch (button.tag) {
+        case 100:{
+            self.selectedIndex = 0;
+            break;
+        }
+        case 101:{
+            self.selectedIndex = 1;
+            break;
+        }
+        case 102:{
+            self.selectedIndex = 2;
+            break;
+        }
+            
+        default:
+            break;
+    }
 }
 
 - (void)didReceiveMemoryWarning {

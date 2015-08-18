eb1588714a19aa0a1d46c31cb1aa0c629e252863
yan2531 <1028183856@qq.com>
2015-08-18 14:56:40 +0800

    storyboard 拖入主要控制器

---
+++ LbsqMerchantVersion/LbsqMerchantVersion/TabbarViewController.m
@@ -0,0 +1,37 @@
+//
+//  TabbarViewController.m
+//  LbsqMerchantVersion
+//
+//  Created by Runkun1 on 15/8/18.
+//  Copyright (c) 2015年 Runkun. All rights reserved.
+//
+
+#import "TabbarViewController.h"
+
+@interface TabbarViewController ()
+
+@end
+
+@implementation TabbarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
